394b629e15dfcdb2606e27ae37a2ae3c9a788ea4
qkrtjdrb1120-ux <qkrtjdrb1120@gmail.com>
2026-03-22 16:22:39 +0900

    Add files via upload

--- ip/myip_watch_timer_1_0/src/exam03_var_odule.v
+++ ip/myip_watch_timer_1_0/src/exam03_var_odule.v
@@ -21,28 +21,6 @@
 
 //edgr detector add work
 
-module edge_detector_p(
-    input clk, reset_p,
-    input cp,
-    output p_edge, n_edge);
-
-    reg ff_cur, ff_old;
-    
-    always @(posedge clk, posedge reset_p)begin
-        if(reset_p)begin
-            ff_cur = 0;
-            ff_old = 0;
-        end
-        else begin
-            ff_old = ff_cur;
-            ff_cur = cp;
-        end
-    end
-    
-    assign p_edge = ({ff_cur, ff_old} == 2'b10) ? 1 : 0;
-    assign n_edge = ({ff_cur, ff_old} == 2'b01) ? 1 : 0;
-
-endmodule
 
 
 module watch(
